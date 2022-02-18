d3c090dfce1b6deeb8ae6882dc907e9ae1a173aa
marieStamp <mariemarkina@gmail.com>
2022-02-18 23:04:36 +0300

    hw4

--- src/userData.ts
+++ src/userData.ts
@@ -14,12 +14,20 @@ localStorage.setItem('favoritesAmount', '5');
 const getDataFromLocalStorage = (key: string): string => {
   return localStorage.getItem(key);
 }
+
+function instanceOfUser(object: any): object is User {
+  return 'userName' in object && 'avatarUrl' in object;
+}
   
 export const getUserData = (): User => {
   const data: unknown = JSON.parse(getDataFromLocalStorage('user'));
-    
-  return data as User;
-};
+  if (instanceOfUser(data)) {
+    return data
+  } else {
+    return {userName: '', avatarUrl: ''};
+  }
+};  
+  
   
 export const getFavoritesAmount = (): number => {
   const data: unknown = getDataFromLocalStorage('favoritesAmount');
